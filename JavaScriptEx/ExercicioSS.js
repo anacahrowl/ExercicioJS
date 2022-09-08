e023a3644385798b6edd8c0ad266be580d50675b
Carolina Tavares <93790048+anacahrowl@users.noreply.github.com>
2022-09-08 13:58:41 -0300

    Add files via upload

---
+++ JavaScriptEx/ExercicioSS.js
@@ -0,0 +1,15 @@
+
+var peso 
+prompt("Qual é o seu peso?");
+var nome 
+prompt("Qual é o seu nome?");
+var sobrenome   
+prompt("Qual é o seu sobrenome?");
+var altura 
+prompt("Qual é sua altura?");
+var dataInternacao  
+prompt("Qual é sua data de internação?");
+var diabetes
+prompt("Você é tem diabetes?");
+
+
---
+++ JavaScriptEx/OperadoresEVariaveis.js
@@ -0,0 +1,20 @@
+
+var variavel = 45 //inteiro
+var variavel = 49.5 //real
+var variavel = "Programadores cariocas"
+var varivel = "A"
+var variavel = false 
+var varivel = true
+
+const pi = 3.1416;
+//operadores aritmedicos 
+//(+, -, *, /, %)
+
+//operadores relacionais 
+// ==, >=, <=, >, <, !=
+
+//operadores logicos 
+// e ou (&& ||)
+
+//incremento e decremento 
+//(++ --)
