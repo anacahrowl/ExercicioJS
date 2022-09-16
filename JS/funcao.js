dc12c3f5efc94ac01a5cec4a18419a3c4d042393
Carolina Tavares <93790048+anacahrowl@users.noreply.github.com>
2022-09-16 11:37:16 -0300

    Add files via upload

---
+++ JS/funcao.js
@@ -0,0 +1,76 @@
+// let n1, n2, resultado
+// n1= prompt("Digite um número: ");
+// n2= prompt("Digite o segundo número: ");
+
+// function abacaxi (n1, n2){
+    // resultado = parseInt(n1) + parseInt(n2);
+    // return alert (resultado);
+
+
+//abacaxi(n1,n2);
+//}
+
+   // do {
+    //opcao = mostrarMenu();
+    //}while(opcao != 2)
+
+   //function mostrarMenu(){
+    //entrada = "";
+    // alert("== Mostrar Menu ==" +
+   //  "\n1 - Mostrar Menu" + 
+    // "\n2 - Sair");
+
+   // return entrada = prompt();
+                  //EXPLICAÇÃO DE FUNCTION//
+//-----------------------------------------------------------
+
+
+//EXERCÍCIOS 
+
+    //let num1, num2, percent
+
+  // num1 = prompt("Digite um número:");
+   // num2= prompt("Qual é a porcentagem: ");
+   // porcentagem(num1, num2)
+   // function porcentagem(num1, num2) {
+      // percent = num1 * (num2 /100)
+      // return  alert("Porcentagem % " + percent);
+    
+
+//}
+// let Celsius, Fahrenheit
+//Celsius = prompt("Digite a temperatura em graus Celsius: ");
+//conversao (Celsius, Fahrenheit)
+//function conversao(Celsius, Fahrenheit){
+ //   Fahrenheit =  ((Celsius) * 9 + (+32))/ 9
+   // return alert(" A temperatura em graus Fahrenheit é de " + Fahrenheit+ "Graus Fahrenheit");
+
+//}
+
+//let Celsius, Fahrenheit
+//Fahrenheit = prompt("Digite a temperatura em graus Fahrenheit");
+//conversao (Celsius, Fahrenheit)
+//function conversao(Celsius, Fahrenheit){
+   // Celsius = ((Fahrenheit - 32) / 9) * 5
+   // return alert(" A temperatura em graus Celsius é de " + Celsius + " graus Celsius");
+
+//}
+
+//let altura, peso, imc
+//altura = prompt("Insira sua altura: ");
+//peso = prompt("Insira seu peso: ");
+
+//imc (altura, peso)
+//function imc (altura, peso){
+
+    //imc = peso / (altura * altura);
+    //return alert("Seu índice de massa coporal é" + imc);
+
+//}
+  
+
+
+
+   
+
+
